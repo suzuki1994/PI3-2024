0e5ec2504688c6400d08930c13a284e279944c7c
suzuki1994 <51233986+suzuki1994@users.noreply.github.com>
2024-09-02 21:15:25 -0300

    Update esp_hx711_test.py

--- Balança/HX711/esp_hx711_test.py
+++ Balança/HX711/esp_hx711_test.py
@@ -1,31 +1,31 @@
-# test of hx711 
+from hx711 import HX711  # Importa a classe HX711 do módulo hx711
 
-from hx711 import HX711 # Importing HX711 class from hx711 module
+# Define os pinos de dados (DT) e o pino de clock serial (SCK) para o HX711
+DATA_PIN = 2  # Pino conectado ao DT (Data)
+SCK_PIN = 4   # Pino conectado ao SCK (Clock)
 
-
-# Define the data pin (DT) and serial clock pin (SCK) for the HX711
-DATA_PIN = 2  # Pin connected to DT
-SCK_PIN = 4   # Pin connected to SCK
-
-# Create an instance of the HX711 object
+# Cria uma instância do objeto HX711
 hx = HX711(DATA_PIN, SCK_PIN)
-hx.set_scale(10)  # Set the scale
-xvar = 0  # Initialize variable to store the known tare weight
+hx.set_scale(10)  # Define a escala do sensor
+xvar = 0  # Inicializa a variável para armazenar o peso da tara conhecida
 
-# Main function
+# Função principal
 while True:
-    hx.tare() # Request known tare weight
-    read=hx.read() #Read raw data
-    average=hx.read_average() # Read the weight avarege 
-    value=hx.make_average() # Read the weight avarege another way with division 
+    hx.tare()  # Solicita a leitura do peso de tara conhecido
+    read = hx.read()  # Lê os dados brutos do sensor
+    average = hx.read_average()  # Lê a média do peso (com base em várias leituras)
+    value = hx.make_average()  # Lê a média do peso de outra forma, utilizando uma divisão
     
-    if xvar == 0:  # If tare weight is not yet stored
-        xvar = value  # Store the tare weight
+    # Se o peso da tara ainda não foi armazenado
+    if xvar == 0:
+        xvar = value  # Armazena o peso da tara
     
-    output = float(value) - float(xvar)  # Calculate net weight
-    
-    print('loop raw: ', read) # Print the raw version 
-    print('loop x samples: ', average) # Print the avarege of x samples
-    print('kg: ', value, '->', output) # Print weight in kilograms
-    print('---------------')
+    output = float(value) - float(xvar)  # Calcula o peso líquido subtraindo a tara
+
+    # Exibe os valores lidos
+    print('loop raw: ', read)  # Imprime o valor bruto da leitura
+    print('loop x samples: ', average)  # Imprime a média de várias amostras
+    print('kg: ', value, '->', output)  # Imprime o peso em quilogramas e o peso líquido calculado
+    print('---------------')  # Imprime um separador para melhor visualização no loop
+
 
